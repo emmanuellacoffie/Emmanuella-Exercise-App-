76ebd2e387746b0189681cf68e9aa6d07c36e7a3
emmanuellacoffie <62953785+emmanuellacoffie@users.noreply.github.com>
2020-04-29 16:26:58 -0400

    Create exerciseh.js

---
+++ exerciseh.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+
+import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Expo,Platform, StyleSheet, Button, Image, TouchableHighlight} from 'react-native';
+
+
+class Exerciseh extends React.Component {
+
+
+  render() {
+     
+    return (
+      
+
+     <View style={styles.containerView}>
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+
+
+
+
+
+</View>
+
+</View>
+</View>
+    );
+  }
+
+}
+
+
+
+
+
+const styles = StyleSheet.create({
+
+containerView: {
+  flex: 1,
+},
+loginScreenContainer: {
+  flex: 1,
+},
+
+
+loginFormView: {
+  flex: 1
+},
+
+
+
+    
+});
+
+
+export default Exerciseh;
